f9ed1a08f9e5d2ae29d69fd654315d4d4f836f9e
AlimardanAhmadov <53153126+AlimardanAhmadov@users.noreply.github.com>
2021-01-03 15:29:11 +0400

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,18 +1,9 @@
 from django.conf.urls import include
-from rest_framework import routers
 from . import views
 from django.urls import path
 
 
-router = routers.DefaultRouter()
-router.register('companies/', views.CompanyViewSet, basename='Company')
-router.register('operations/', views.OperationViewSet, basename='Operation')
-router.register('cards/', views.CardViewSet, basename='Card')
-router.register('bank_headers/', views.BankCalculationHeadersViewSet, basename='Headers'),
-
-
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('', views.CompanyList.as_view(), name='home'),
     path('əməliyyat/<str:pk>/', views.OperationList.as_view(), name='operations'),
     path('əməliyyat/siyahı/<str:pk>/', views.CardList.as_view(), name='card_list'),
