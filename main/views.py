28e3f7733e9dd04c1025f4e2dd532a1dfc9fe5a0
Alimardan <53153126+AlimardanAhmadov@users.noreply.github.com>
2021-01-04 12:47:19 +0400

    Update views.py

--- main/views.py
+++ main/views.py
@@ -8,7 +8,6 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView 
 from rest_framework.decorators import api_view
-from django.views.generic import TemplateView
 from django.core.exceptions import ValidationError
 from django.db.models import FloatField, Sum
 from django.db.models.functions import Cast
