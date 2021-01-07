cf0d7e1c60e73e91be691ba49edb0f6d6ea5f2ae
Alimardan <53153126+AlimardanAhmadov@users.noreply.github.com>
2021-01-07 15:26:24 +0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('user.urls')),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
